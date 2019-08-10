ee2d391ab10e679a1720cd4a95a12bb722537b16
Chiien <fuchienhsu12@gmail.com>
2019-08-10 16:18:59 -0300

    Feat(integrations/details.test.js) change to /api/items

--- __tests__/integrations/details.test.js
+++ __tests__/integrations/details.test.js
@@ -5,7 +5,7 @@ const server = require('../../src/server');
 describe('Details', () => {
     it('should return item details by id', async () => {
         const id = 'MLA795803301';
-        const response = await request(server).get(`/items/${id}`);
+        const response = await request(server).get(`/api/items/${id}`);
         expect(response.status).toBe(200);
     });
 });
