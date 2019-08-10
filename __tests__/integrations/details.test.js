05ae0546f047f3ebab65231dcd46221e48b89a24
Chiien <fuchienhsu12@gmail.com>
2019-08-10 16:06:30 -0300

    Feat(integrations/details.test.js) add first test

---
+++ __tests__/integrations/details.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+
+const server = require('../../src/server');
+
+describe('Details', () => {
+    it('should return item details by id', async () => {
+        const id = 'MLA795803301';
+        const response = await request(server).get(`/items/${id}`);
+        expect(response.status).toBe(200);
+    });
+});
