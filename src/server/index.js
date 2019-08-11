323fa7868572ca91a4639eedaa5cf54715f16d3d
Chiien <fuchienhsu12@gmail.com>
2019-08-10 22:43:56 -0300

    Feat(src/server) add middlewares cors

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,20 @@ class App {
     constructor() {
         this.express = express();
 
+        this.middlewares();
         this.routes();
+    }
+
+    middlewares() {
+        this.express.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET');
+            res.header(
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With, Content-Type, Accept'
+            );
+            next();
+        });
     }
 
     routes() {
