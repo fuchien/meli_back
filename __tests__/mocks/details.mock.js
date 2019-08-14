56e7a4269492e0cea04c1f418e1c8f448942ae11
Chiien <fuchienhsu12@gmail.com>
2019-08-14 10:05:23 -0300

    Feat(mocks/details.mock.js) add mock

---
+++ __tests__/mocks/details.mock.js
@@ -0,0 +1,280 @@
+module.exports = {
+    detailsMock: {
+        id: 'MLA795803301',
+        site_id: 'MLA',
+        title: 'Reproductor Apple iPod Touch 32gb (7 Generacion) Gtia ',
+        subtitle: null,
+        seller_id: 133799638,
+        category_id: 'MLA7262',
+        official_store_id: null,
+        price: 26599,
+        base_price: 26599,
+        original_price: null,
+        currency_id: 'ARS',
+        initial_quantity: 2,
+        available_quantity: 1,
+        sold_quantity: 1,
+        sale_terms: [
+            {
+                id: 'WARRANTY_TIME',
+                name: 'Tiempo de garantía',
+                value_id: null,
+                value_name: '90 días',
+                value_struct: {
+                    number: 90,
+                    unit: 'días'
+                }
+            },
+            {
+                id: 'WARRANTY_TYPE',
+                name: 'Tipo de garantía',
+                value_id: '2230280',
+                value_name: 'Garantía del vendedor',
+                value_struct: null
+            }
+        ],
+        buying_mode: 'buy_it_now',
+        listing_type_id: 'gold_special',
+        start_time: '2019-06-28T23:45:36.000Z',
+        stop_time: '2039-06-23T04:00:00.000Z',
+        condition: 'new',
+        permalink:
+            'https://articulo.mercadolibre.com.ar/MLA-795803301-reproductor-apple-ipod-touch-32gb-7-generacion-gtia-_JM',
+        thumbnail:
+            'http://mla-s2-p.mlstatic.com/961788-MLA31657666807_082019-I.jpg',
+        secure_thumbnail:
+            'https://mla-s2-p.mlstatic.com/961788-MLA31657666807_082019-I.jpg',
+        pictures: [
+            {
+                id: '961788-MLA31657666807_082019',
+                url:
+                    'http://mla-s2-p.mlstatic.com/961788-MLA31657666807_082019-O.jpg',
+                secure_url:
+                    'https://mla-s2-p.mlstatic.com/961788-MLA31657666807_082019-O.jpg',
+                size: '275x500',
+                max_size: '275x500',
+                quality: ''
+            },
+            {
+                id: '637245-MLA31273191701_062019',
+                url:
+                    'http://mla-s2-p.mlstatic.com/637245-MLA31273191701_062019-O.jpg',
+                secure_url:
+                    'https://mla-s2-p.mlstatic.com/637245-MLA31273191701_062019-O.jpg',
+                size: '500x389',
+                max_size: '1200x934',
+                quality: ''
+            }
+        ],
+        video_id: null,
+        descriptions: [
+            {
+                id: 'MLA795803301-2161977662'
+            }
+        ],
+        accepts_mercadopago: true,
+        non_mercado_pago_payment_methods: [],
+        shipping: {
+            mode: 'me2',
+            free_methods: [
+                {
+                    id: 73328,
+                    rule: {
+                        default: true,
+                        free_mode: 'country',
+                        free_shipping_flag: true,
+                        value: null
+                    }
+                }
+            ],
+            tags: ['mandatory_free_shipping'],
+            dimensions: null,
+            local_pick_up: true,
+            free_shipping: true,
+            logistic_type: 'xd_drop_off',
+            store_pick_up: false
+        },
+        international_delivery_mode: 'none',
+        seller_address: {
+            city: {
+                name: 'Tribunales'
+            },
+            state: {
+                id: 'AR-C',
+                name: 'Capital Federal'
+            },
+            country: {
+                id: 'AR',
+                name: 'Argentina'
+            },
+            search_location: {
+                city: {
+                    id: 'TUxBQ0NBUGZlZG1sYQ',
+                    name: 'Capital Federal'
+                },
+                state: {
+                    id: 'TUxBUENBUGw3M2E1',
+                    name: 'Capital Federal'
+                }
+            },
+            latitude: -34.60048,
+            longitude: -58.379288,
+            id: 146780144
+        },
+        seller_contact: null,
+        location: {},
+        geolocation: {
+            latitude: -34.60048,
+            longitude: -58.379288
+        },
+        coverage_areas: [],
+        attributes: [
+            {
+                id: 'BRAND',
+                name: 'Marca',
+                value_id: '9344',
+                value_name: 'Apple',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'CONNECTOR_TYPE',
+                name: 'Tipo de conector',
+                value_id: null,
+                value_name: '1 x Lightning Connector',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'DATA_STORAGE_CAPACITY',
+                name: 'Capacidad de almacenamiento de datos',
+                value_id: null,
+                value_name: '32 GB',
+                value_struct: {
+                    number: 32,
+                    unit: 'GB'
+                },
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'INCLUDES_HEADPHONES',
+                name: 'Incluye auriculares',
+                value_id: '242085',
+                value_name: 'Sí',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'ITEM_CONDITION',
+                name: 'Condición del ítem',
+                value_id: '2230284',
+                value_name: 'Nuevo',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'LINE',
+                name: 'Línea',
+                value_id: null,
+                value_name: 'Touch',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'MODEL',
+                name: 'Modelo',
+                value_id: null,
+                value_name: 'MVHW2LL/A',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'PLAYER_TYPE',
+                name: 'Tipo de reproductor',
+                value_id: '6862362',
+                value_name: 'MP3',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'WITH_BLUETOOTH',
+                name: 'Con Bluetooth',
+                value_id: '242085',
+                value_name: 'Sí',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'WITH_CAMERA',
+                name: 'Con cámara',
+                value_id: '242085',
+                value_name: 'Sí',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'WITH_TOUCHSCREEN',
+                name: 'Con pantalla táctil',
+                value_id: '242085',
+                value_name: 'Sí',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            }
+        ],
+        warnings: [],
+        listing_source: '',
+        variations: [
+            {
+                id: 39512036712,
+                price: 26599,
+                attribute_combinations: [
+                    {
+                        id: 'COLOR',
+                        name: 'Color',
+                        value_id: '52049',
+                        value_name: 'Negro',
+                        value_struct: null
+                    }
+                ],
+                available_quantity: 1,
+                sold_quantity: 1,
+                sale_terms: [],
+                picture_ids: [
+                    '961788-MLA31657666807_082019',
+                    '637245-MLA31273191701_062019'
+                ],
+                catalog_product_id: null
+            }
+        ],
+        status: 'active',
+        sub_status: [],
+        tags: [
+            'good_quality_picture',
+            'good_quality_thumbnail',
+            'brand_verified',
+            'immediate_payment',
+            'cart_eligible'
+        ],
+        warranty: 'Garantía del vendedor: 90 días',
+        catalog_product_id: null,
+        domain_id: 'MLA-DIGITAL_PORTABLE_MEDIA_PLAYERS',
+        parent_item_id: null,
+        differential_pricing: null,
+        deal_ids: [],
+        automatic_relist: false,
+        date_created: '2019-06-28T23:45:36.000Z',
+        last_updated: '2019-08-12T14:53:29.000Z',
+        health: 0.81
+    }
+};
