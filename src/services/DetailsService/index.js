b3386b5ee81144192041205a48d7fb1073355817
Chiien <fuchienhsu12@gmail.com>
2019-08-14 10:41:09 -0300

    Feat(services/DetailsService) add mock if useMock

--- src/services/DetailsService/index.js
+++ src/services/DetailsService/index.js
@@ -1,8 +1,17 @@
 const { Api } = require('../api');
 
+// Variables
+const variables = require('../../config/variables');
+
+// Mocks
+const { mockDescription, mockDetails } = require('./mock');
+
 class DetailsService {
     async findById(id) {
         try {
+            if (variables.useMock) {
+                return mockDetails;
+            }
             return await Api.findById(id);
         } catch (err) {
             return err;
@@ -11,6 +20,9 @@ class DetailsService {
 
     async findDescriptionById(id) {
         try {
+            if (variables.useMock) {
+                return mockDescription;
+            }
             return await Api.findDescriptionById(id);
         } catch (err) {
             return err;
