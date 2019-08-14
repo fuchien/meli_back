9c9c17358951fadd262192cdd86278f261f7283b
Chiien <fuchienhsu12@gmail.com>
2019-08-14 10:24:19 -0300

    Feat(integrations/details.test.js) add mock and new test validator

--- __tests__/integrations/details.test.js
+++ __tests__/integrations/details.test.js
@@ -1,18 +1,15 @@
 const request = require('supertest');
 const { DetailsService } = require('../../src/services/DetailsService');
 
+// nocks
+const { detailsMock } = require('../mocks/details.mock');
+
 jest.mock('../../src/services/DetailsService');
 
 const server = require('../../src/server');
 
 describe('Details', () => {
     it('should return item details and description by id', async () => {
-        const detailsMock = {
-            title: 'Reproductor Apple iPod Touch 32gb (7 Generacion) Gtia ',
-            seller_id: 133799638,
-            price: 18999,
-            sold_quantity: 1
-        };
         const descriptionMock = {
             plain_text:
                 'Aspectos Destacados Del Producto\n\nPantalla Retina IPS Multi-Touch de 4 "\nResolución nativa de 1136 x 640 a 326 ppi'
@@ -27,9 +24,8 @@ describe('Details', () => {
         const response = await request(server).get(`/api/items/${id}`);
 
         expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty('details');
-        expect(response.body.details).toEqual(detailsMock);
-        expect(response.body.description).toEqual(descriptionMock);
+        expect(response.body).toHaveProperty('author');
+        expect(response.body).toHaveProperty('item');
     });
 
     it('should return error to get details', async () => {
@@ -43,5 +39,11 @@ describe('Details', () => {
         const response = await request(server).get(`/api/items/${id}`);
         expect(response.status).toBe(500);
         expect(response.body).toEqual(detailsMock);
+    });
+
+    it('should return error bad request', async () => {
+        const query = 'MLA795803301';
+        const response = await request(server).get(`/api/items/${query}`);
+        expect(response.status).toBe(400);
     });
 });
