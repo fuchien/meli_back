3f603c5ca20b8b974de24579032afa995741295c
Chiien <fuchienhsu12@gmail.com>
2019-08-14 10:41:17 -0300

    Feat(services/ProductsService) add mock if useMock

--- src/services/ProductsService/index.js
+++ src/services/ProductsService/index.js
@@ -1,8 +1,17 @@
 const { Api } = require('../api');
 
+// Variables
+const Variables = require('../../config/variables');
+
+// Mock
+const { mockProducts } = require('./mock');
+
 class ProductsService {
     async findByQuery(query) {
         try {
+            if (Variables.useMock) {
+                return mockProducts;
+            }
             return await Api.findByQuery(query);
         } catch (err) {
             return err;
