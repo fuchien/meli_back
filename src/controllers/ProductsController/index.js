2efd7b90388ec4db3fcd06dd0457bc9c6feac940
Chiien <fuchienhsu12@gmail.com>
2019-08-14 10:45:05 -0300

    Feat(controllers/ProductsController) add model to mapper response object

--- src/controllers/ProductsController/index.js
+++ src/controllers/ProductsController/index.js
@@ -1,11 +1,16 @@
+// SERVICES
 const { ProductsService } = require('../../services/ProductsService');
+
+// MODELS
+const { Products } = require('../../models/Products');
 
 class ProductsController {
     async index(req, res) {
         try {
             const query = req.query.q;
             const products = await ProductsService.findByQuery(query);
-            return res.status(200).json(products);
+            const response = Products(products).getResponse();
+            return res.status(200).json(response);
         } catch (err) {
             return res.status(500).json(err);
         }
