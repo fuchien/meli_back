1819fefee073922ab623a4ce6862cb437b199255
Chiien <fuchienhsu12@gmail.com>
2019-08-14 10:40:35 -0300

    Feat(services/DetailsService/mock.js) add mock

---
+++ src/services/DetailsService/mock.js
@@ -0,0 +1,295 @@
+module.exports = {
+    mockDetails: {
+        id: 'MLA795803301',
+        site_id: 'MLA',
+        title: 'Reproductor Apple iPod Touch 32gb (7 Generacion) Gtia ',
+        subtitle: null,
+        seller_id: 133799638,
+        category_id: 'MLA7262',
+        official_store_id: null,
+        price: 18999,
+        base_price: 18999,
+        original_price: null,
+        currency_id: 'ARS',
+        initial_quantity: 2,
+        available_quantity: 1,
+        sold_quantity: 1,
+        sale_terms: [
+            {
+                id: 'WARRANTY_TIME',
+                name: 'Tiempo de garantía',
+                value_id: null,
+                value_name: '90 días',
+                value_struct: {
+                    number: 90,
+                    unit: 'días'
+                }
+            },
+            {
+                id: 'WARRANTY_TYPE',
+                name: 'Tipo de garantía',
+                value_id: '2230280',
+                value_name: 'Garantía del vendedor',
+                value_struct: null
+            }
+        ],
+        buying_mode: 'buy_it_now',
+        listing_type_id: 'gold_special',
+        start_time: '2019-06-28T23:45:36.000Z',
+        stop_time: '2039-06-23T04:00:00.000Z',
+        condition: 'new',
+        permalink:
+            'https://articulo.mercadolibre.com.ar/MLA-795803301-reproductor-apple-ipod-touch-32gb-7-generacion-gtia-_JM',
+        thumbnail:
+            'http://mla-s2-p.mlstatic.com/961788-MLA31657666807_082019-I.jpg',
+        secure_thumbnail:
+            'https://mla-s2-p.mlstatic.com/961788-MLA31657666807_082019-I.jpg',
+        pictures: [
+            {
+                id: '961788-MLA31657666807_082019',
+                url:
+                    'http://mla-s2-p.mlstatic.com/961788-MLA31657666807_082019-O.jpg',
+                secure_url:
+                    'https://mla-s2-p.mlstatic.com/961788-MLA31657666807_082019-O.jpg',
+                size: '275x500',
+                max_size: '275x500',
+                quality: ''
+            },
+            {
+                id: '637245-MLA31273191701_062019',
+                url:
+                    'http://mla-s2-p.mlstatic.com/637245-MLA31273191701_062019-O.jpg',
+                secure_url:
+                    'https://mla-s2-p.mlstatic.com/637245-MLA31273191701_062019-O.jpg',
+                size: '500x389',
+                max_size: '1200x934',
+                quality: ''
+            }
+        ],
+        video_id: null,
+        descriptions: [
+            {
+                id: 'MLA795803301-2161977662'
+            }
+        ],
+        accepts_mercadopago: true,
+        non_mercado_pago_payment_methods: [],
+        shipping: {
+            mode: 'me2',
+            free_methods: [
+                {
+                    id: 73328,
+                    rule: {
+                        default: true,
+                        free_mode: 'country',
+                        free_shipping_flag: true,
+                        value: null
+                    }
+                }
+            ],
+            tags: ['mandatory_free_shipping'],
+            dimensions: null,
+            local_pick_up: true,
+            free_shipping: true,
+            logistic_type: 'xd_drop_off',
+            store_pick_up: false
+        },
+        international_delivery_mode: 'none',
+        seller_address: {
+            city: {
+                name: 'Tribunales'
+            },
+            state: {
+                id: 'AR-C',
+                name: 'Capital Federal'
+            },
+            country: {
+                id: 'AR',
+                name: 'Argentina'
+            },
+            search_location: {
+                city: {
+                    id: 'TUxBQ0NBUGZlZG1sYQ',
+                    name: 'Capital Federal'
+                },
+                state: {
+                    id: 'TUxBUENBUGw3M2E1',
+                    name: 'Capital Federal'
+                }
+            },
+            latitude: -34.604664,
+            longitude: -58.38625,
+            id: 146780144
+        },
+        seller_contact: null,
+        location: {},
+        geolocation: {
+            latitude: -34.604664,
+            longitude: -58.38625
+        },
+        coverage_areas: [],
+        attributes: [
+            {
+                id: 'BRAND',
+                name: 'Marca',
+                value_id: '9344',
+                value_name: 'Apple',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'CONNECTOR_TYPE',
+                name: 'Tipo de conector',
+                value_id: null,
+                value_name: '1 x Lightning Connector',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'DATA_STORAGE_CAPACITY',
+                name: 'Capacidad de almacenamiento de datos',
+                value_id: null,
+                value_name: '32 GB',
+                value_struct: {
+                    number: 32,
+                    unit: 'GB'
+                },
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'INCLUDES_HEADPHONES',
+                name: 'Incluye auriculares',
+                value_id: '242085',
+                value_name: 'Sí',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'ITEM_CONDITION',
+                name: 'Condición del ítem',
+                value_id: '2230284',
+                value_name: 'Nuevo',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'LINE',
+                name: 'Línea',
+                value_id: null,
+                value_name: 'Touch',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'MODEL',
+                name: 'Modelo',
+                value_id: null,
+                value_name: 'MVHW2LL/A',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'PLAYER_TYPE',
+                name: 'Tipo de reproductor',
+                value_id: '6862362',
+                value_name: 'MP3',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'WITH_BLUETOOTH',
+                name: 'Con Bluetooth',
+                value_id: '242085',
+                value_name: 'Sí',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'WITH_CAMERA',
+                name: 'Con cámara',
+                value_id: '242085',
+                value_name: 'Sí',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            },
+            {
+                id: 'WITH_TOUCHSCREEN',
+                name: 'Con pantalla táctil',
+                value_id: '242085',
+                value_name: 'Sí',
+                value_struct: null,
+                attribute_group_id: 'OTHERS',
+                attribute_group_name: 'Otros'
+            }
+        ],
+        warnings: [],
+        listing_source: '',
+        variations: [
+            {
+                id: 39512036712,
+                price: 18999,
+                attribute_combinations: [
+                    {
+                        id: 'COLOR',
+                        name: 'Color',
+                        value_id: '52049',
+                        value_name: 'Negro',
+                        value_struct: null
+                    }
+                ],
+                available_quantity: 1,
+                sold_quantity: 1,
+                sale_terms: [],
+                picture_ids: [
+                    '961788-MLA31657666807_082019',
+                    '637245-MLA31273191701_062019'
+                ],
+                catalog_product_id: null
+            }
+        ],
+        status: 'active',
+        sub_status: [],
+        tags: [
+            'brand_verified',
+            'good_quality_picture',
+            'good_quality_thumbnail',
+            'immediate_payment',
+            'cart_eligible'
+        ],
+        warranty: 'Garantía del vendedor: 90 días',
+        catalog_product_id: null,
+        domain_id: 'MLA-DIGITAL_PORTABLE_MEDIA_PLAYERS',
+        parent_item_id: null,
+        differential_pricing: null,
+        deal_ids: [],
+        automatic_relist: false,
+        date_created: '2019-06-28T23:45:36.000Z',
+        last_updated: '2019-08-10T03:03:47.000Z',
+        health: 0.81
+    },
+
+    mockDescription: {
+        text: '',
+        plain_text:
+            'Aspectos Destacados Del Producto\n\nPantalla Retina IPS Multi-Touch de 4 "\nResolución nativa de 1136 x 640 a 326 ppi\nProcesador de fusión A10\nCámara trasera de 8MP 1080p\nCámara frontal de 1.2MP 720p\n32 GB de almacenamiento\nWi-Fi 5 (802.11ac) + Bluetooth 4.1\nSistema operativo iOS 12\nApple Arcade + Grupo FaceTime\nIncluye Apple EarPods\n\nDescripción general de Apple iPod Touch 7ª generación:\n\nDiseñado para algo más que música, el iPod touch de 32 GB de 7ª generación de Apple ofrece experiencias de juegos de realidad aumentada inmersiva, le brinda la posibilidad de conectarse con amigos a través del Grupo FaceTime y le brinda acceso a todo lo que la App Store tiene para ofrecer. Dentro del iPod touch está el chip A10 Fusion.\n\nEl A10 ha sido diseñado para tener el suficiente impacto en el procesamiento para brindarle un rendimiento fluido mientras ejecuta los últimos juegos móviles y experiencias de realidad aumentada.\n\nCuando se trata de música, el iPod touch te brinda 32 GB de espacio de almacenamiento para que siempre tengas tus canciones favoritas a mano. También tiene la posibilidad de suscribirse a Apple Music para obtener millones de opciones de transmisión. Escuche con algunos auriculares Bluetooth (opcionales), o conecte los Apple EarPods incluidos en la toma de auriculares incorporada.\n\nEl iPod touch también cuenta con una cámara trasera y frontal para tomar fotos HDR, videos de 1080p y conectarse con amigos a través de Group FaceTime. Independientemente de cómo se divierta con el iPod touch, lo reproducirá y administrará todo a través de una pantalla Retina de 4 "nítida y vívida.\n\nJuegos + Apple Arcade\nCon el chip A10 Fusion y la brillante pantalla Retina de 4 ", el iPod touch de Apple se convierte en una potente solución para juegos móviles que se ve genial. Su soporte para aplicaciones y juegos AR le brinda la posibilidad de superponer su diversión digital a su entorno real. Haz que los monstruos caminen por el patio de recreo, compitan con un amigo en un escritorio o destruyan las naves espaciales desde el cielo sobre ti.\n\nEl iPod touch de séptima generación también es compatible con Apple Arcade. Es un servicio de suscripción de juegos de Apple que le brinda acceso ilimitado a más de 100 juegos nuevos sin anuncios ni compras dentro de la aplicación. Puedes jugarlos en línea o sin conexión, y la experiencia se puede transferir a todos tus dispositivos Apple. Juegue su juego favorito en su nuevo iPod touch y luego bájelo en su Apple TV cuando llegue a casa.\n\nGrupo FaceTime\nCon la cámara incorporada, puedes usar tu iPod touch para FaceTime con un amigo o Group FaceTime con hasta 32 amigos al mismo tiempo. El mosaico de quien está hablando se hace más grande automáticamente, por lo que es fácil seguir la conversación. Incluso puede saltar a una sesión de Group FaceTime directamente desde un texto de grupo en Mensajes.\n\nMensajes\nEl iPod touch es compatible con Wi-Fi 5 (802.11ac) para acceder a Internet, a la App Store y a otros servicios en línea, como los mensajes. Con los mensajes, puedes enviar textos gratuitos que están llenos de efectos animados, pegatinas y más. Envíelos a cualquier persona que use un iPod touch, iPhone, iPad o Mac. Mensajes le permite iniciar conversaciones en grupo, silenciar conversaciones, compartir enlaces, ubicaciones y más.\n\nTiempo de pantalla\nLa aplicación Screen Time es una excelente manera de monitorear y administrar las experiencias digitales de sus hijos. Puedes usarlo en su nuevo iPod touch o en cualquier otro dispositivo Apple. Establezca límites de tiempo y excepciones, controle el acceso a las aplicaciones, las capacidades de compra dentro de la aplicación y los sitios web en los que pueden ser pocos. Incluso puede realizar un seguimiento de su ubicación durante todo el día. Es una solución ideal para enseñar a los niños a usar los dispositivos digitales de una manera saludable y responsable.\n\nCaracterísticas adicionales\nSoporte de Siri: cuando está conectado a través de Wi-Fi, puede usar Siri para enviar mensajes, configurar recordatorios, responder preguntas y más\nHasta 40 horas de reproducción de música.\nHasta 8 horas de reproducción de video.\nAirPlay Mirroring de fotos y video a Apple TV (2ª generación y superior)\nGraba videos a cámara lenta a 120 fps\n\nEn la caja:\nIPod touch de 32 GB de Apple (7ª generación, gris espacial)\nApple EarPods\nCable Lightning a USB',
+        last_updated: '2019-06-29T01:14:46.000Z',
+        date_created: '2019-06-28T23:45:36.000Z',
+        snapshot: {
+            url:
+                'http://descriptions.mlstatic.com/D-MLA795803301.jpg?hash=8520c3b8559cb08aa7e782b8f5334ffe_0x0',
+            width: 0,
+            height: 0,
+            status: ''
+        }
+    }
+};
