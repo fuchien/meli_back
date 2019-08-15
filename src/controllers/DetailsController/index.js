ecc9a785de2ed817ab9272ba36cca1f03f3adf94
Chiien <fuchienhsu12@gmail.com>
2019-08-15 10:53:18 -0300

    Feat(detailsController) change response obj

--- src/controllers/DetailsController/index.js
+++ src/controllers/DetailsController/index.js
@@ -11,7 +11,7 @@ class DetailsController {
             const details = await DetailsService.findById(id);
             const description = await DetailsService.findDescriptionById(id);
             const response = Details({ details, description }).getResponse();
-            response.success = true;
+            // response.success = true;
             return res.status(200).json(response);
         } catch (err) {
             return res.status(500).json(err);
