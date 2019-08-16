2df693314f7c9c9a23872ccff201f92435854a93
Chiien <fuchienhsu12@gmail.com>
2019-08-16 11:47:35 -0300

    Feat(server) add dotenv and process env

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const routes = require('../routes');
 
@@ -11,7 +12,10 @@ class App {
 
     middlewares() {
         this.express.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
+            res.header(
+                'Access-Control-Allow-Origin',
+                process.env.MELI_API_CORS_DOMAIN
+            );
             res.header('Access-Control-Allow-Methods', 'GET');
             res.header(
                 'Access-Control-Allow-Headers',
